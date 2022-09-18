8d1f881a3c8b6e11909e18c33e09258c472e9864
nowgyung <vetibeti@naver.com>
2022-09-18 23:21:11 +0900

    220918

--- shop/src/main/java/com/shop/exception/OutOfStockException.java
+++ shop/src/main/java/com/shop/exception/OutOfStockException.java
@@ -1,2 +1,9 @@
-package com.shop.exception;public class OutOfStockException {
+package com.shop.exception;
+
+public class OutOfStockException extends RuntimeException{
+
+    public OutOfStockException(String message){
+        super(message);
+    }
+
 }
